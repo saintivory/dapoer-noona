0fd298632f80f3b2c9920242460286a36f1c4b11
Ivo Adnan <rich.ivoadnan@gmail.com>
2026-03-22 22:03:51 +0700

    Update site title to Dapoer Noona

--- app/head.tsx
+++ app/head.tsx
@@ -3,7 +3,6 @@ export default function Head() {
     <>
       <title>Dapoer Noona</title>
       <meta name="description" content="Website Katering Dapoer Noona" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" />
     </>
   )
